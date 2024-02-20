b2027ce00fd09d555bb26ac3ca031371c582fd72
Urmi25 <farjanaurmi25@gmail.com>
2024-02-20 18:39:19 +0600

    CDP Assignment-02

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ const nav = document.getElementsByClassName('nav')[0];
 
 toggleButton.addEventListener('click', ()=> {
     nav.classList.toggle('active');
+   
     
+})
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
 })
